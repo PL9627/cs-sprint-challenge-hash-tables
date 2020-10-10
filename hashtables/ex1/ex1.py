a95895872f30a1f303d9913f5b66ca3e59135695
Paul Lee <paul.lee0627@gmail.com>
2020-10-09 17:38:28 -0700

    finally figured out ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    store = {}
 
+    for i, weight in enumerate(weights):
+        store[weight] = i
+
+    for i, weight in enumerate(weights):
+        if limit - weight in store:
+            j = store[limit - weight]
+            
+            if i > j:
+                return (i, j)
+            else:
+                return(j, i)
+    
     return None
